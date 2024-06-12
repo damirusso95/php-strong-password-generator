8fa8f7c1499a83f88e89d3590a4cc0ae93821654
damirusso95 <damirusso95@gmail.com>
2024-06-12 15:54:04 +0200

    M2: creo una stringa di caratteri- strlen() per recuperare la lunghezza della stringa- rand() sul ciclo for per genere un numero casuale tra 0 e la lunghezza della stringa dei caratteri

--- generate_password.php
+++ generate_password.php
@@ -8,16 +8,23 @@
 <body>
     <h1>La tua password</h1>
     <?php
-    // operatore di coalescenza nulla (??) verifica se una variabile è definita e non è null
+        // operatore di coalescenza nulla (??) verifica se una variabile è definita e non è null
     $length = $_GET['length'] ?? null;
-    
+
     if($length !== null) {
         $password = '';
+
+        // caratteri consentiti
+        $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|;:",.<>?/`~';
+        // recuperiamo la lunghezza della stringa di caratteri
+        $charactersLength = strlen($characters);
     
 
-    for ($i = 0; $i < $length; $i++) {
-        $password .= 'X';
-    }
+        for ($i = 0; $i < $length; $i++) {
+            // rand() genere un numero casuale tra 0 e la lunghezza della stringa dei caratteri -1
+            $randomIndex = rand(0, $charactersLength - 1);
+            $password .= $characters[$randomIndex];
+        }
     echo "<p>La tua password è: $password</p>";
     }
     ?>
